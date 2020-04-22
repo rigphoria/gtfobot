0aa0d86cab28c9205c0531474c9d1edbbabedebb
ryan warnick <rigphoria@gmail.com>
2020-04-22 12:42:55 -0400

    removed !readydm because you can message the bot directly

--- bot.js
+++ bot.js
@@ -39,20 +39,6 @@ client.on('message', message => {
             reply += " are ready to play";
             message.channel.send(reply);
         }
-    } else if(message.content === '!readydm'){
-        if(players.length === 1){
-            message.author.send(players[0].username + " is ready to play");
-        } else {
-            let reply = "";
-            for(let i = 0; i < players.length; i++){
-                reply += players[i].username;
-                if(i < players.length - 1){
-                    reply += ", ";
-                }
-            }
-            reply += " are ready to play";
-            message.author.send(reply);
-        }
     } else if(message.content === "!commands"){
         message.channel.send("Commands: !qtfo or !queue to add yourself to the queue. !removeme to remove yourself from the queue. !ready to get a list of players currently in the queue.");
     }
